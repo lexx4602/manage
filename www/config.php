82e134fbf4ed877651188437cbed18cc272c2bed
lexx4602 <s_os@inbox.ru>
2018-02-13 20:46:09 +0300

    Finish day

--- www/config.php
+++ www/config.php
@@ -1,2 +1,13 @@
 <?php
 
+db['name']='manage';
+db['host']='localhost';
+db['user']='root';
+db['passwd']='';
+app['WebRoot']='/manage';
+app['AppRoot']='/home/user/manage';
+#app['']='';
+
+
+
+
--- www/index.php
+++ www/index.php
@@ -1,1 +1,9 @@
 <?php
+require_once 'config.php';
+	$dbh = new PDO ("mysql:dbname=$db['name'];host=$db['host'],
+		$db['login'],
+		$db['passwd'],
+		array(PDO::MYSQL_ATTR-INIT_COMMAND => "SET NAMES 'utf8'")
+	);
+
+
