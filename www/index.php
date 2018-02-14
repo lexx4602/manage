15263b090615b4aa869c231b57ab4da2f5715dfe
lexx4602 <s_os@inbox.ru>
2018-02-14 14:13:08 +0300

    YII2 Installed

--- www/index.php
+++ www/index.php
@@ -1,9 +1,13 @@
 <?php
-require_once 'config.php';
-	$dbh = new PDO ("mysql:dbname=$db['name'];host=$db['host'],
-		$db['login'],
-		$db['passwd'],
-		array(PDO::MYSQL_ATTR-INIT_COMMAND => "SET NAMES 'utf8'")
-	);
 
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/../../../../local/share/yii/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
 
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
